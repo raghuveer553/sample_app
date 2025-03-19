f156eabf17b6404c5167b98a34ac159f34f41133
Naveen <raghuveer553@gmail.com>
2025-03-19 14:29:47 +0530

    login and redirect to user page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     # Find the user by email
-    debugger
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #Login and show user's home page and test change
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
     else
+      flash[:danger] = "Email and password do not match"
       render "new", status: :unprocessable_entity
+    end
   end
 end
