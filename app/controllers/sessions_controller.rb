cdb6dd32ddff94c5774cd6fdceaa9a4eab36e5d1
Naveen <raghuveer553@gmail.com>
2025-03-19 13:52:56 +0530

    login functionality added in action #create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # Find the user by email
+    debugger
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Login and show user's home page and test change
+    else
+      render "new", status: :unprocessable_entity
+  end
 end
